f6fc9a2acf1f68f1139d1ef9455a0dcd44f110d1
anoopn <anoopn.03@gmail.com>
2019-05-25 00:44:20 +0300

    Test for Main

---
+++ backend/src/test/java/com/ats/application/spring/oauth/angular/LoginSpringOauthAngularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ats.application.spring.oauth.angular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginSpringOauthAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
